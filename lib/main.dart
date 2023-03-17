1d17c0337c84ae4ed6a2a25d6e44e3a04b03b5ac
Jonas Wanke <contact@jonas-wanke.com>
2023-03-17 21:11:22 +0100

    Use path URL strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:flutter_web_plugins/url_strategy.dart';
+
 import 'app/_.dart';
 import 'settings/_.dart';
 
 Future<void> main() async {
+  usePathUrlStrategy();
   await initApp();
   await initSettings();
 
