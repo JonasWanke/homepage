f323fd6c3ec7cab0e6b394b581a790c125fb3751
Jonas Wanke <contact@jonas-wanke.com>
2023-03-30 13:10:03 +0200

    Change primary color to green

--- lib/app/widgets/app.dart
+++ lib/app/widgets/app.dart
@@ -10,14 +10,14 @@ import 'konami_wrapper.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PreferenceBuilder<Brightness>(
+    return PreferenceBuilder(
       preference: services.preferences.brightness,
       builder: (context, brightness) => KonamiWrapper(
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Jonas Wanke',
-          theme: _createTheme(Brightness.light),
-          darkTheme: _createTheme(Brightness.dark),
+          theme: createTheme(Brightness.light),
+          darkTheme: createTheme(Brightness.dark),
           themeMode: brightness.isLight ? ThemeMode.light : ThemeMode.dark,
           onGenerateRoute: router.onGenerateRoute,
           navigatorObservers: [createLoggingNavigatorObserver()],
@@ -25,20 +25,26 @@ class App extends StatelessWidget {
       ),
     );
   }
-}
 
-ThemeData _createTheme(Brightness brightness) {
-  return ThemeData(
-    brightness: brightness,
-    useMaterial3: true,
-    textTheme: TextTheme(
-      headlineMedium: TextStyle(color: brightness.mediumEmphasisOnColor),
-    ),
-    cardTheme: const CardTheme(
-      margin: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(cardBorderRadius)),
+  static ThemeData createTheme(Brightness brightness) {
+    const primaryColor = Colors.green;
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSwatch(
+        primarySwatch: primaryColor,
+        backgroundColor:
+            primaryColor.withOpacity(0.02).alphaBlendOn(brightness.color),
+        brightness: brightness,
       ),
-    ),
-  );
+      textTheme: TextTheme(
+        headlineMedium: TextStyle(color: brightness.mediumEmphasisOnColor),
+      ),
+      cardTheme: const CardTheme(
+        margin: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(cardBorderRadius)),
+        ),
+      ),
+    );
+  }
 }
